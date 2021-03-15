ee74a785225a09b1b5418a35aeb4281dc37746bb
Tigran Nalbandyan <grig199922@inbox.ru>
2021-03-15 22:42:20 +0400

    Added PriorityHeap DS.

---
+++ DataStructures/PriorityHeap.cpp
@@ -0,0 +1,134 @@
+/* Compile command: g++ -o PriorityHeap.exe -std=c++11 -static-libgcc -static-libstdc++ .\PriorityHeap.cpp */
+
+#define PARENT(i) (i - 1) / 2
+#define LEFT(i) (2 * i) + 1
+#define RIGHT(i) (2 * i) + 2
+#define INF 1e9
+
+#include "iostream"
+
+using namespace std;
+
+class PQueue {
+    int n, *data, length;
+
+public:
+    PQueue();
+    void init(int);
+    void max_heapify(int);
+    void build_max_heap();
+    void display();
+    int get_maximum();
+    int extract_max();
+    void increase_key(int, int);
+    void insert(int);
+    ~PQueue();
+};
+
+PQueue::PQueue() {
+    length = 0;
+    data = NULL;
+}
+
+void PQueue::init(int size) {
+    length = size - 1;
+    data = new int[2 * size];
+}
+
+void PQueue::max_heapify(int i) {
+    int l = LEFT(i);
+    int r = RIGHT(i);
+    int largest = i;
+
+    if (LEFT(i) <= length && data[l] > data[largest]) {
+        largest = l;
+    }
+    if (RIGHT(i) <= length && data[r] > data[largest]) {
+        largest = r;
+    }
+
+    if (largest != i) {
+        int temp = data[i];
+        data[i] = data[largest];
+        data[largest] = temp;
+        max_heapify(largest);
+    }
+}
+
+void PQueue::build_max_heap() {
+    for (int i = PARENT(length); i >= 0; i--) {
+        max_heapify(i);
+    }
+}
+
+void PQueue::display() {
+    if (length < 1) {
+        cout << "no elements" << endl;
+    } else {
+        cout << "elements are:" << endl;
+        for (int i = 0; i < length; i++) {
+            cout << data[i] << endl;
+        }
+    }
+}
+
+int PQueue::get_maximum() {
+    return data[0];
+}
+
+int PQueue::extract_max() {
+    int m = data[0];
+    data[0] = data[length];
+    length--;
+    max_heapify(0);
+    return m;
+}
+
+void PQueue::increase_key(int i, int key) {
+    if (data[i] > key) {
+        cout << "error: new key is smaller than current key" << endl;
+    } else {
+        data[i] = key;
+        while (data[PARENT(i)] < data[i] && i > 0) {
+            int temp = data[PARENT(i)];
+            data[PARENT(i)] = data[i];
+            data[i] = temp;
+            i = PARENT(i);
+        }
+    }
+}
+
+void PQueue::insert(int key) {
+    length++;
+    data[length] = -INF;
+    increase_key(length, key);
+}
+
+PQueue::~PQueue()
+{
+    delete[] data;
+}
+
+int main() {
+	PQueue pqueue;
+    int max;
+
+	pqueue.init(0);
+	pqueue.display();
+
+    pqueue.insert(1);
+    pqueue.insert(3);
+    pqueue.insert(2);
+    pqueue.insert(10);
+    pqueue.increase_key(2, 5);
+
+	pqueue.build_max_heap();
+	pqueue.display();
+
+    cout << "maximum element is " << pqueue.get_maximum() << endl;
+    max = pqueue.extract_max();
+    cout << "extracted maximum element is " << max << endl;
+    cout << "maximum element is " << pqueue.get_maximum() << endl;
+
+    return 0;
+}
